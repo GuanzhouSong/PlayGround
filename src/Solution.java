ee176265253da0483b7c0078da126a4f608c4602
Guanzhou <guanzhou.song@gmail.com>
2020-05-20 15:06:23 -0400

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,25 @@
-public class Solution {
+import java.util.Stack;
 
+class Solution {
+
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[T.length];
+        for (int i = 0; i < T.length; i++) {
+            int cur = T[i];
+            if(stack.isEmpty() || cur<= stack.peek()){
+                stack.push(i);
+            }else{
+                while(!stack.isEmpty() && cur>stack.peek()){
+                    int prev = stack.pop();
+                    res[prev] = i - prev;
+                }
+                stack.push(i)
+;            }
+
+
+        }
+
+        return res;
+    }
 }
