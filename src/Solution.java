9c48f752b3196c2f46962d751c5e8ac7e0ae68f5
Guanzhou Song <guanzhou.song@gmail.com>
2020-06-20 09:25:13 -0400

    sss

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,56 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode cur = root;
-        List<Integer> res = new ArrayList<>();
-        while(cur != null){
-            res.add(cur.val);
-            if(cur.right!=null){
-                stack.push(cur.right);
-            }
-            cur = cur.left;
-            if(cur == null && !stack.isEmpty()){
-                cur = stack.pop();
+
+    class Point {
+
+        int x, y, val;
+
+        Point(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+    }
+
+    public int rectangleArea(int[][] rectangles) {
+        int M = 1000000007;
+        List<Point> data = new ArrayList<>();
+        for (int[] r : rectangles) {
+            data.add(new Point(r[0], r[1], 1));
+            data.add(new Point(r[0], r[3], -1));
+            data.add(new Point(r[2], r[1], -1));
+            data.add(new Point(r[2], r[3], 1));
+        }
+        data.sort((a, b) -> (a.x - b.x));
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int preX = -1;
+        int preY = -1;
+        int result = 0;
+        for (int i = 0; i < data.size(); i++) {
+            Point p = data.get(i);
+            map.put(p.y, map.getOrDefault(p.y, 0) + p.val);
+            if (i == data.size() - 1 || data.get(i + 1).x > p.x) {
+                if (preX > -1) {
+                    result += ((long) preY * (p.x - preX)) % M;
+                    result %= M;
+                }
+                preY = calcY(map);
+                preX = p.x;
             }
         }
+        return result;
+    }
 
-        return res;
+    private int calcY(TreeMap<Integer, Integer> map) {
+        int result = 0, pre = -1, count = 0;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (pre >= 0 && count > 0) {
+                result += e.getKey() - pre;
+            }
+            count += e.getValue();
+            pre = e.getKey();
+        }
+        return result;
     }
 }
---
+++ src/SolutionTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    Solution s;
+    @org.junit.Before
+    public void setUp() throws Exception {
+        s = new Solution();
+    }
+
+    @org.junit.Test
+    public void maxTurbulenceSize() {
+        System.out.println();
+    }
+}
